8c7b80d6ed07a5834d39d4bc27ece4884bab876f
Jeffrey Way <jeffrey@jeffrey-way.com>
2021-08-31 13:15:48 -0400

    Refactor Collections

---
+++ src/Collection.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Laracasts\Transcriptions;
+
+use ArrayAccess;
+use ArrayIterator;
+use Countable;
+use IteratorAggregate;
+use JsonSerializable;
+use Traversable;
+
+class Collection implements Countable, IteratorAggregate, ArrayAccess, JsonSerializable
+{
+    public function __construct(protected array $items)
+    {
+        //
+    }
+
+    public function map(callable $fn): self
+    {
+        return new static(
+            array_map($fn, $this->items)
+        );
+    }
+
+    public function offsetUnset($key)
+    {
+        unset($this->items[$key]);
+    }
+
+    public function offsetGet($key)
+    {
+        return $this->items[$key];
+    }
+
+    public function offsetExists($key)
+    {
+        return isset($this->items[$key]);
+    }
+
+    public function getIterator(): Traversable
+    {
+        return new ArrayIterator($this->items);
+    }
+
+    public function offsetSet($key, $value)
+    {
+        if (is_null($key)) {
+            $this->items[] = $value;
+        } else {
+            $this->items[$key] = $value;
+        }
+    }
+
+    public function count(): int
+    {
+        return count($this->items);
+    }
+
+    public function jsonSerialize()
+    {
+        return $this->items;
+    }
+}
--- src/Lines.php
+++ src/Lines.php
@@ -2,71 +2,16 @@
 
 namespace Laracasts\Transcriptions;
 
-use ArrayAccess;
-use ArrayIterator;
-use Countable;
-use IteratorAggregate;
-use JsonSerializable;
-use Traversable;
-
-class Lines implements Countable, IteratorAggregate, ArrayAccess, JsonSerializable
+class Lines extends Collection
 {
-    public function __construct(protected array $lines)
-    {
-        //
-    }
-
     public function asHtml(): string
     {
-        $formattedLines = array_map(
-            fn(Line $line) => $line->toHtml(),
-            $this->lines
-        );
-
-        return (new static($formattedLines))->__toString();
-    }
-
-    public function count(): int
-    {
-        return count($this->lines);
-    }
-
-    public function getIterator(): Traversable
-    {
-        return new ArrayIterator($this->lines);
+        return $this->map(fn(Line $line) => $line->toHtml())
+            ->__toString();
     }
 
     public function __toString(): string
     {
-        return implode("\n", $this->lines);
-    }
-
-    public function offsetExists($key)
-    {
-        return isset($this->lines[$key]);
-    }
-
-    public function offsetGet($key)
-    {
-        return $this->lines[$key];
-    }
-
-    public function offsetSet($key, $value)
-    {
-        if (is_null($key)) {
-           $this->lines[] = $value;
-        } else {
-            $this->lines[$key] = $value;
-        }
-    }
-
-    public function offsetUnset($key)
-    {
-        unset($this->lines[$key]);
-    }
-
-    public function jsonSerialize()
-    {
-        return $this->lines;
+        return implode("\n", $this->items);
     }
 }
