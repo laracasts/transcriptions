248bbcd5e0c3b803412c85a20f063e1dbf8c2bbb
Jeffrey Way <jeffrey@jeffrey-way.com>
2021-09-01 16:51:10 -0400

    Return numeric check
    Some VTT files do not include an index for each card.

--- src/Transcription.php
+++ src/Transcription.php
@@ -6,7 +6,7 @@ class Transcription
 {
     public function __construct(protected array $lines)
     {
-        $this->lines = array_slice(array_filter(array_map('trim', $lines)), 1);
+        $this->lines = $this->normalizeLines($lines);
     }
 
     public static function load(string $path): self
@@ -18,8 +18,15 @@ class Transcription
     {
         return new Lines(array_map(
             fn($line) => new Line(...$line),
-            array_chunk($this->lines, 3)
+            array_chunk($this->lines, 2)
         ));
+    }
+
+    protected function normalizeLines(array $lines): array
+    {
+        $lines = array_slice(array_filter(array_map('trim', $lines)), 1);
+
+        return array_filter($lines, fn($line) => !is_numeric($line));
     }
 
     public function __toString(): string
